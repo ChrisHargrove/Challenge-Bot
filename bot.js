9995b9409417c8b5abf9b7ea03b761572861d0a7
Chris <hargrove.chris91@gmail.com>
2021-04-10 14:07:20 +0100

    Dynamic commands

--- bot.js
+++ bot.js
@@ -1,15 +1,36 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
+client.commands = new Discord.Collection();
+
+//Dynamically load all commands from their respective files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
 client.on('ready', () => {
-    console.log('Logged in as ${client.user.tag}!');
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+    if (!msg.content.startsWith('!') || msg.author.bot) return;
+
+    const args = msg.content.slice(1).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(msg, args);
     }
+    catch (error) {
+        console.error(error);
+        msg.reply("Something wen't wrong whilst executing that command");
+    }
+
 });
 
 client.login(process.env.token);
