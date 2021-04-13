e9ab4a33f5bec3d64e2410808ba8e432de00ae4b
Chris <hargrove.chris91@gmail.com>
2021-04-13 21:16:52 +0100

    Fixing stuff

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const Client = new Discord.Client();
 
 var Guild = null;
 var DatabaseChannel = null;
 
-client.commands = new Discord.Collection();
+Client.commands = new Discord.Collection();
 
-loadAllCommands(client);
+loadAllCommands();
 
-client.on('ready', () => {
-    if (checkGuild(client)) {
+Client.on('ready', () => {
+    if (checkGuild()) {
         checkDatabaseChannel();
 
         DatabaseChannel.send("Beep Boop! Setting Up Database")
@@ -20,16 +20,16 @@ client.on('ready', () => {
     }
 });
 
-client.on('message', msg => {
+Client.on('message', msg => {
     if (!msg.content.startsWith('!') || msg.author.bot) return;
 
     const args = msg.content.slice(1).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (!client.commands.has(command)) return;
+    if (!Client.commands.has(command)) return;
 
     try {
-        client.commands.get(command).execute(msg, args);
+        Client.commands.get(command).execute(msg, args);
     }
     catch (error) {
         console.error(error);
@@ -38,26 +38,26 @@ client.on('message', msg => {
 
 });
 
-client.login(process.env.token);
+Client.login(process.env.token);
 
 //Utility Functions
-function loadAllCommands(client) {
+function loadAllCommands() {
     const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
         const command = require(`./commands/${file}`);
-        client.commands.set(command.name, command);
+        Client.commands.set(command.name, command);
     }
 }
 
-function checkGuild(client) {
-    var guild = client.guilds.find(guild => guild.name === "Modelling Roundup");
+function checkGuild() {
+    var guild = Client.guilds.cache.find(guild => guild.name === "Modelling Roundup");
     if (guild != null) {
         Guild = guild;
         return true;
     }
 
     console.log("Bot is being used in incorrect Guild! Shutting Down!...");
-    client.destroy();
+    Client.destroy();
     return false;
 }
 
